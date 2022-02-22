c7da881895e82762ecd7551c8fe7184457a2c673
Rahulgoy <rahulkumargoyal00@gmail.com>
2022-02-22 11:43:30 +0530

    Firebase setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// // Store
+import { Provider } from 'react-redux';
+import store from './store/store';
+// routes
+import Router from './routes';
+// theme
+import ThemeConfig from './theme';
+import GlobalStyles from './theme/globalStyles';
+// components
+import ScrollToTop from './components/ScrollToTop';
+import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  return (
+    <Provider store={store}>
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeConfig>
+    </Provider>
+  );
+}
---
+++ src/config/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: 'AIzaSyDlh8fdpOIas9sFLquKbIbGoRyN6fomzLU',
+  authDomain: 'face-recognition-51469.firebaseapp.com',
+  databaseURL: 'https://face-recognition-51469-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'face-recognition-51469',
+  storageBucket: 'face-recognition-51469.appspot.com',
+  messagingSenderId: '1018849117258',
+  appId: '1:1018849117258:web:16b8689ce10c064d0d52b3'
+});
+
+firebase.firestore().settings({ timestampsInSnapshots: true });
+export const db = firebaseConfig.firestore();
+
+export default firebase;
