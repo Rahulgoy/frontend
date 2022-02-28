3443c32f52f8e8b357667018a4da91af8d74d49f
Rahulgoy <rahulkumargoyal00@gmail.com>
2022-02-28 22:42:12 +0530

    Different In & Out Tables

--- src/pages/ViewAttendance.js
+++ src/pages/ViewAttendance.js
@@ -30,136 +30,15 @@ import {UserListHeadAttendance} from '../sections/@dashboard/user/UserListHeadCu
 import USERLIST from '../_mocks_/user';
 import { getDatabase, ref, onValue} from "firebase/database";
 import * as XLSX from 'xlsx';
-// ----------------------------------------------------------------------
-
-const TABLE_HEAD = [
-  { id: 'rollNumber', label: 'Roll Number', alignRight: false },
-  { id: 'name', label: 'Name', alignRight: false },
-  { id: 'hostel', label: 'Hostel', alignRight: false },
-  { id: 'Date',label:'Date',alignRight:false },
-  { id: 'Time',label:'Time',alignRight:false },
-  { id: 'Status',label:'Status',alignRight:false },
-];
-
+import AttendanceTables from './AttendanceTables';
 // ----------------------------------------------------------------------
 
 
-
-
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function applySortFilter(array, comparator, query) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  if (query) {
-    return filter(array, (_user) => _user.Name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-  }
-  return stabilizedThis.map((el) => el[0]);
-}
 
 export default function ViewAttendance() {
-  const [finalDate, setFinalDate] = useState(null);
-  const [page, setPage] = useState(0);
-  const [order, setOrder] = useState('asc');
-  const [selected, setSelected] = useState([]);
-  const [orderBy, setOrderBy] = useState('name');
-  const [filterName, setFilterName] = useState('');
-  const [rowsPerPage, setRowsPerPage] = useState(5);
   const [students, setStudents] = useState([]);
-  
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = students.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-    setSelected(newSelected);
-  };
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleFilterByName = (event) => {
-    setFilterName(event.target.value);
-  };
-
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
-
- 
-  var filteredUsers = applySortFilter(students, getComparator(order, orderBy), filterName);
-  if(finalDate){
-    // console.log(user.Date.toString()===finalDate.toString())
-    // console.log("Called: ",filteredUsers)
-    const filUsers = []
-    filteredUsers.filter(user => user.Date.toString()===finalDate.toString()).map(us=>filUsers.push(us))
-    // console.log("After: ",filUsers)
-    filteredUsers = filUsers
-  }
-  const isUserNotFound = filteredUsers.length === 0;
-  
-  
-  const downloadExcel=()=>{
-    console.log("Download")
-    const workSheet=XLSX.utils.json_to_sheet(filteredUsers)
-    const workBook=XLSX.utils.book_new()
-    XLSX.utils.book_append_sheet(workBook,workSheet,"students")
-    // Buffer
-    let buf = XLSX.write(workBook,{bookType:"xlsx",type:"buffer"})
-    // Binary string
-    XLSX.write(workBook,{bookType:"xlsx",type:"binary"})
-    // 
-    XLSX.writeFile(workBook,"StudentAttendance.xlsx"); 
-
-  }
+  const [inStudents, setInStudents] = useState([]);
+  const [outStudents, setOutStudents] = useState([]);
   
   
   useEffect(() => {
@@ -174,7 +53,15 @@ export default function ViewAttendance() {
       Object.keys(data).forEach(key => { 
         arr.push(data[key]);
       })
+      // console.log(arr);
       setStudents(arr);
+      const inArr = [];
+      const outArr = [];
+      arr.filter(user => user.IsOut===false).map(us=>inArr.push(us));
+      arr.filter(user => user.IsOut===true).map(us=>outArr.push(us));
+
+      setInStudents(inArr);
+      setOutStudents(outArr);
     })
   }, []);
 
@@ -188,98 +75,30 @@ export default function ViewAttendance() {
             Attendance
           </Typography>
         </Stack>
-        <Card>
-          <UserListToolbar
-            numSelected={selected.length}
-            filterName={filterName}
-            onFilterName={handleFilterByName}
-            setFinalDate={setFinalDate}
-            downloadExcel = {downloadExcel}
-            component="Attendance"
-            
-          />
-          <Scrollbar>
-            <TableContainer sx={{ minWidth: 800 }}>
-              <Table>
-                <UserListHeadAttendance
-                  order={order}
-                  orderBy={orderBy}
-                  headLabel={TABLE_HEAD}
-                  rowCount={students.length}
-                  numSelected={selected.length}
-                  onRequestSort={handleRequestSort}
-                  onSelectAllClick={handleSelectAllClick}
-                />
-                <TableBody>
-                  {filteredUsers
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row) => {
-                      const { id, Name, rollNumber, imageUrl, Hostel, Date, Time, IsOut } = row;
-                      const isItemSelected = selected.indexOf(Name) !== -1;
-                      return (
-                        <TableRow
-                          key={id}
-                          tabIndex={-1}
-                          role="checkbox"
-                          selected={isItemSelected}
-                          aria-checked={isItemSelected}
-                        >
-                          <TableCell>
-                          </TableCell>
-                          <TableCell component="th" scope="row" padding="none">
-                            <Stack direction="row" alignItems="center" spacing={2}>
-                              <Avatar alt={Name} src={imageUrl} />
-                              <Typography variant="subtitle2" noWrap>
-                                {rollNumber}
-                              </Typography>
-                            </Stack>
-                          </TableCell>
-                          <TableCell align="left">{Name}</TableCell>
-                          <TableCell align="left">{Hostel}</TableCell>
-                          <TableCell align="left">{Date}</TableCell>
-                          <TableCell align="left">{Time}</TableCell>
-                          <TableCell>{IsOut?"Out":"In"}</TableCell>
-                          {/* <TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell> */}
-                          {/* <TableCell align="left">
-                            <Label
-                              variant="ghost"
-                              color={(status === 'banned' && 'error') || 'success'}
-                            >
-                              {sentenceCase(status)}
-                            </Label>
-                          </TableCell> */}
-                        </TableRow>
-                      );
-                    })}
-                  {emptyRows > 0 && (
-                    <TableRow style={{ height: 53 * emptyRows }}>
-                      <TableCell colSpan={6} />
-                    </TableRow>
-                  )}
-                </TableBody>
-                {isUserNotFound && (
-                  <TableBody>
-                    <TableRow>
-                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                        <SearchNotFound searchQuery={filterName} />
-                      </TableCell>
-                    </TableRow>
-                  </TableBody>
-                )}
-              </Table>
-            </TableContainer>
-          </Scrollbar>
-
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={students.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Card>
+        <AttendanceTables students={inStudents}
+                          />
+        <AttendanceTables students={outStudents}
+                          />
+        
+        {/* <AttendanceTables selected={selected}
+                          filterName={filterName}
+                          filteredUsers={filteredUsers}
+                          handleFilterByName={handleFilterByName}
+                          setFinalDate={setFinalDate}
+                          downloadExcel={downloadExcel}
+                          order={order}
+                          orderBy={orderBy}
+                          students={students}
+                          handleRequestSort={handleRequestSort}
+                          handleSelectAllClick={handleSelectAllClick}
+                          emptyRows={emptyRows}
+                          isUserNotFound={isUserNotFound}
+                          rowsPerPage={rowsPerPage}
+                          page={page}
+                          handleChangePage={handleChangePage}
+                          TABLE_HEAD={TABLE_HEAD}
+                          handleChangeRowsPerPage={handleChangeRowsPerPage} /> */}
+          
       </Container>
     </Page>
   );
